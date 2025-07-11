8bda00c73221f93263e53e5f870f34d71644a684
DecodingMaru <marupriyanshu3@gmail.com>
2025-07-11 13:06:45 +0530

    port running

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,jsonify,request 
 from model3 import Predict
+import os
 app=Flask(__name__)
 
 @app.route("/")
@@ -37,5 +38,6 @@ def predict():
         return f"Error: {e}"
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

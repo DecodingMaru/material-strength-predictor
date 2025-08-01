57874bd66cad51bc1b5f537dc109c85a93faa14e
DecodingMaru <marupriyanshu3@gmail.com>
2025-08-01 13:18:06 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 import pickle
 
-data = pd.read_csv("prediction/data/Alloys.csv")
+data = pd.read_csv("/data/Alloys.csv")
 
 X = data.drop(["Alloy", "Tensile Strength: Ultimate (UTS) (psi)", "Melting Completion (Liquidus)"], axis=1)
 y = data["Tensile Strength: Ultimate (UTS) (psi)"]
